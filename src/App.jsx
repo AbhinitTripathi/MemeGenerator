62131f263b866302e8658de1ccf87bc58a4169b9
Abhinit Tripathi <114798873+AbhinitTripathi@users.noreply.github.com>
2025-07-28 04:36:38 +0000

    basic layout and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css'
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
 
 export default function App() {
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit
-    </main>
+    <>
+      <Header />
+      <Main />
+    </>
   )
 }
